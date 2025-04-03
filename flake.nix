73df20567b766bdeac8c5357d9f66a030a617c0f
Fernando Perry <fernandoperry1234@gmail.com>
2025-04-03 23:17:06 +0700

    update flake

--- flake.nix
+++ flake.nix
@@ -12,15 +12,34 @@
                 pkgs = nixpkgs.legacyPackages.${system};
             in
                 {
+                packages.default = pkgs.stdenv.mkDerivation {
+                    pname = "battrem";
+                    version = "0.1";
+                    src = self;
+
+                    nativeBuildInputs = with pkgs; [
+                        meson
+                        ninja
+                        pkg-config
+                    ];
+
+                    buildInputs = with pkgs; [
+                        libnotify
+                    ];
+
+                    meta = {
+                        description = "A simple battery reminder written in C";
+                        homepage = "https://github.com/commrade-goad/battrem";
+                    };
+                };
+
                 devShells.default = pkgs.mkShell {
                     buildInputs = with pkgs; [
-                        # Build tools
                         meson
                         ninja
                         pkg-config
                         gcc
 
-                        # Dependencies
                         libnotify
                         glibc
                     ];
